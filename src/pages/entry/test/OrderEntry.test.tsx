4ed818fa9f1734f7091a0cac2377941165c2bacf
LeleoSouza <leobrausa@hotmail.com>
2021-12-12 22:17:19 -0500

    addea waitFor test, and exact:false

--- src/pages/entry/test/OrderEntry.test.tsx
+++ src/pages/entry/test/OrderEntry.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { OrderEntry } from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
-test('should handle errors for scoop and toppings routes ', async () => {
+test('should handle errors for scoop and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
       res(ctx.status(500));
@@ -12,6 +12,8 @@ test('should handle errors for scoop and toppings routes ', async () => {
     })
   );
   render(<OrderEntry />);
-  const alerts = await screen.findAllByRole('alert', { name: 'An unexpected error accured. Please try again later.' });
-  expect(alerts).toHaveLength(2);
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole('alert');
+    expect(alerts).toHaveLength(2);
+  });
 });
---
+++ src/pages/entry/test/TotalUpdates.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Options } from '../Options';
+describe('Total updates test', () => {
+  test('should Update scoop total when scoop changes', async () => {
+    render(<Options optionsType='scoops' />);
+    //   Make sure totoal starts out @ $0.00
+    const scoopSubTotal = screen.getByText('Scoops total: $', { exact: false });
+    expect(scoopSubTotal).toHaveTextContent('0.00');
+
+    //   update vanilla scoop to 1 and check input
+    const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' });
+    // make sure there is not input before
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, '1');
+    expect(scoopSubTotal).toHaveTextContent('2.00');
+
+    // update chocolate scoops to 2 and check output
+    const chocolateInput = await screen.findByRole('spinbutton', { name: 'Chocolate' });
+    userEvent.clear(chocolateInput);
+    userEvent.type(chocolateInput, '2');
+    expect(scoopSubTotal).toHaveTextContent('6.00');
+  });
+});
